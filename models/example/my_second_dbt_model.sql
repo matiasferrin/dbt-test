263ff1c95730f5b2a9225d4b90feb2f681b2ce82
matiasferrin <matiasferrin@gmail.com>
2023-10-22 17:38:47 +0200

    new model

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,7 +1,7 @@
 
 -- Use the `ref` function to select from other models
 
-select *
-from employee_dataset.employees
+select '123' as test_field, e.*
+from employee_dataset.employees as e
 --{{ ref('my_first_dbt_model') }}
 where name = 'Carol'
