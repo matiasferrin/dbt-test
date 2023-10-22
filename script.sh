9306ff42c79f7db68782a17b3a0775078b4ee487
matiasferrin <matiasferrin@gmail.com>
2023-10-22 17:24:17 +0200

    deleted dbt test

--- script.sh
+++ script.sh
@@ -2,4 +2,3 @@
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
 dbt run --target dev --profiles-dir .
-dbt test --data --target dev --profiles-dir .
